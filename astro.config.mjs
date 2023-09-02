6498fa3a6aee2be5e60a6a5ae50642b55b63a491
renzorr <benaventer01@gmail.com>
2023-09-01 19:37:53 -0500

    fix astro config issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  applyBaseStyles: false,
-  experimental: {
-    assets: true
-  }
+  applyBaseStyles: false
 });
